cfd6f692ccff5e49a88cea705d8407afc220fc97
koteyevlev <entrant@epd540bt7fbf5pchhbu9.auto.internal>
2019-08-22 14:14:25 +0000

    POST WORKgit add .!

--- models.py
+++ models.py
@@ -17,18 +17,12 @@ class Citizen(db.Model):
     name = db.Column(db.String(257))
     birth_date = db.Column(db.DateTime)
     gender = db.Column(db.String(7))
-    relatives = db.Column(db.Integer(1000))
+    relatives = db.Column(db.PickleType())
     import_id = db.Column(db.Integer, unique=True, primary_key=True)
-    slug = db.Column(db.String(257), unique=True)
     created = db.Column(db.DateTime, default=datetime.now)
 
     def __init__(self, *args, **kwards):
         super(Citizen, self).__init__(*args, **kwards)
-        self.generate_slug()
-
-    def generate_slug(self):
-        if self.citizen_id:
-            self.slug = slugify(self.citizen_id)
 
     def __repr__(self):
         return '<Post id: {}, name: {}>'.format(self.citizen_id, self.name)
--- view.py
+++ view.py
@@ -2,6 +2,8 @@ from app import app
 from flask import Flask, jsonify, render_template, request
 import json
 import flask
+import re
+import datetime
 from models import Citizen
 from app import db
 
@@ -15,9 +17,10 @@ def first_validate():
         return None, errors
 
     for field_name in ['citizen_id', 'town', 'street', 'building', 'apartment', 'name', 'birth_date', 'gender', 'relatives']:
-        if type(json_s.get(field_name)) is not str:
+        if not type(json_s.get(field_name)):
             errors.append(
                 "Field '{}' is missing or is not a string".format(field_name))
+            print("I found this", field_name)
 
     return json_s, errors
 
@@ -44,24 +47,31 @@ def post_data():
     json_s, errors = first_validate()
     #errors= last_valid(errors)
     if errors:
-        #return errors[0]
-        return flask.Response(status=400), 400
-    try:
+        print(errors[0])
+        return errors[0], 400
+        # return flask.Response(status=400), 400
+#    try:
+    if True == True:
         db.create_all()
-        citizen_id = request.form['citizen_id']
-        town = request.form['town']
-        street = request.form['street']
-        building = request.form['building']
-        apartment = request.form['apartment']
-        name = request.form['name']
-        birth_date = request.form['birth_date']
-        gender = request.form['gender']
-        relatives = request.form['relatives']
+        citizen_id = json_s['citizen_id']
+        town = json_s['town']
+        street = json_s['street']
+        building = json_s['building']
+        apartment = json_s['apartment']
+        name = json_s['name']
+        #birth_date = json_s['birth_date']
+        #match = re.search(r'\d{2}-\d{2}-\d{4}', json_s['birth_date'])
+        birth_date = datetime.datetime.strptime(json_s['birth_date'], '%d.%m.%Y').date()
+        gender = json_s['gender']
+        relatives = list(map(int, str(json_s['relatives'])[1:-1].split(',')))
+        # return "stupid citizens crash again"
         citizen = Citizen(citizen_id=citizen_id, town=town, street=street, building=building,
                           apartment=apartment, name=name, birth_date=birth_date, gender=gender,
                           relatives=relatives)
+        # return "Base crush"
         db.session.add(citizen)
         db.session.commit()
-        return jsonify( "data" = citizen.import_id), 201
-    except:
-        return 'Something wrong', 400
+        #return "something"
+        return jsonify( {"data": citizen.import_id}), 201
+    #except:
+     #   return 'Something wrong', 400
