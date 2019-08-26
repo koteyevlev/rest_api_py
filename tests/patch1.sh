98c2d38918c70e483cfcc932e02ecd591547cd26
koteyevlev <entrant@epd540bt7fbf5pchhbu9.auto.internal>
2019-08-26 10:47:40 +0000

    more tests

--- tests/patch1.sh
+++ tests/patch1.sh
@@ -1,1 +1,1 @@
-curl -d '{"birth_date": "27.02.2019"}' -H "Content-Type: application/json"  -X PATCH 'http://0.0.0.0:8080/imports/2/citizens/2'
+curl -d '{"birth_date": "27.02.2019", "relatives": [8, 9]}' -H "Content-Type: application/json"  -X PATCH 'http://0.0.0.0:8080/imports/17/citizens/6'
