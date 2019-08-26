d0d0ea983c070880e8cb2927d2c2834fd3162469
koteyevlev <entrant@epd540bt7fbf5pchhbu9.auto.internal>
2019-08-26 11:10:15 +0000

    more comments

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
+## При установке на стороннюю ВМ нужно прописать путь к flask окружению для работы
 source /home/entrant/venvs/flaskproj/bin/activate;
 cd /home/entrant/rest_api_py;
 gunicorn --bind 0.0.0.0:8080 --threads 20 main:app
